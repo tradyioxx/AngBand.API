c8b5ac132e5e8f7c3fab68b20748492ad50860d7
Sébastien DUPAS <se.dupas@orange.fr>
2020-07-24 15:07:27 +0200

    Initial Commit

---
+++ Controllers/AddController.cs
@@ -0,0 +1,17 @@
+﻿using AngBand.Core2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngBang.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddController : ControllerBase
+    {
+        [HttpGet]
+        public long Get(int x, int y)
+        {
+            var addition = new Addition(x, y);
+            return addition.Resultat;
+        }
+    }
+}
---
+++ Controllers/SumController.cs
@@ -0,0 +1,17 @@
+﻿using AngBand.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngBang.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SumController : ControllerBase
+    {
+        [HttpGet]
+        public long Get([FromQuery] int[] n)
+        {
+            var somme = new Somme(n);
+            return somme.Resultat;
+        }
+    }
+}
